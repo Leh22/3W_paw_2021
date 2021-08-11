1916064117c6cd8c60c0cd3ee37decc24ac3f0f9
Leh22 <alefe0santos@gmail.com>
2021-08-11 15:02:59 -0300

    Primeira alteração

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const servidor = express()
+
+servidor.get('/', (req, res, next) => {
+    return res.send({
+        "mensagem": "Bem-vindo ao servidor NodeJS"
+    })
+})
+
+servido.listen(3000, () => {
+    console.log('Servidor Funcionando')
+})
